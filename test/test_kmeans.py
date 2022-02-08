b9ff8b6a43cf21112b6172560f9d3255164ca239
shirondru <59421145+shirondru@users.noreply.github.com>
2022-02-07 21:54:38 -0800

    Fix indentation errors

--- test/test_kmeans.py
+++ test/test_kmeans.py
@@ -5,104 +5,105 @@ from scipy.spatial.distance import cdist
 import pytest
 
 def test_check_easy_cluster():
-	"""
-	This test checks to see that the KMeans algorithm properly clusters a very easily clusterable dataset.
-	I use make_clusters with k=3 and a very low scale to ensure the 3 clusters are high distinct. 
-	I then loop through each of these truth clusters and identify the samples in these clusters. I then check what cluster(s)
-	the KMeans algorithm predicted the same samples are in, and I assert that they are all in only one cluster; because the dataset
-	is so easily clusterable, it should have perfectly clustered these samples, even if it didn't name the clusters in the same way.
-	"""
+    """
+    This test checks to see that the KMeans algorithm properly clusters a very easily clusterable dataset.
+    I use make_clusters with k=3 and a very low scale to ensure the 3 clusters are high distinct. 
+    I then loop through each of these truth clusters and identify the samples in these clusters. I then check what cluster(s)
+    the KMeans algorithm predicted the same samples are in, and I assert that they are all in only one cluster; because the dataset
+    is so easily clusterable, it should have perfectly clustered these samples, even if it didn't name the clusters in the same way.
+    """
 
-	# create tight clusters
-	k = 3
+    # create tight clusters
+    k = 3
     clusters, labels = make_clusters(k=k,scale=0.3)
 
     #fit KMeans and predictions for these samples
     km = KMeans(k=k,random_state = 0)
-	km.fit(clusters)
-	pred = km.predict(clusters)
+    km.fit(clusters)
+    pred = km.predict(clusters)
 
     # for each true cluster, create 
     for clust in range(k):
-	    samples_in_true_cluster = labels==clust 
-	    pred_labels_for_true_cluster = pred[samples_in_true_cluster,] #get the predicted cluster labels for the same samples
-	    
-	     #check the predicted cluster labels for all samples truly in one cluster are actually in just one cluster
-	    assert len(np.unique(pred_labels_for_true_cluster)) == 1, "Your model was not able to properly cluster an easy dataset in the same way as the truth"
-	    
+        samples_in_true_cluster = labels==clust 
+        pred_labels_for_true_cluster = pred[samples_in_true_cluster,] #get the predicted cluster labels for the same samples
+        
+         #check the predicted cluster labels for all samples truly in one cluster are actually in just one cluster
+        assert len(np.unique(pred_labels_for_true_cluster)) == 1, "Your model was not able to properly cluster an easy dataset in the same way as the truth"
+        
 
 def test_centroid_init_randomness():
-	"""
-	Although the centroid initialization `self._centroid_init` is implemented via the KMeans++ method, the very first initialized centroid is chosen randomly from one of the 
-	data points, and which point is chosen for the first initial centroid will affect the initialization of the rest of the centroids.
-	If this initialization is implemented correctly, it should be pseudo-random, and varying the random seed will affect the centroid initialization.
-	Assert that is the case
-	"""
-	k=5
-	clusters, labels = make_clusters(k=k,scale=1.5)
-	centroid_mats = []
-	for seed in [12312,23423523,1313158291]:
-		km = KMeans(k=k,random_state = seed)
-		centroid_mats.append(km._centroid_init(clusters)) #append 2D matrix with centroids to list
+    """
+    Although the centroid initialization `self._centroid_init` is implemented via the KMeans++ method, the very first initialized centroid is chosen randomly from one of the 
+    data points, and which point is chosen for the first initial centroid will affect the initialization of the rest of the centroids.
+    If this initialization is implemented correctly, it should be pseudo-random, and varying the random seed will affect the centroid initialization.
+    Assert that is the case
+    """
+    k=5
+    clusters, labels = make_clusters(k=k,scale=1.5)
+    centroid_mats = []
+    for seed in [12312,23423523,1313158291]:
+        km = KMeans(k=k,random_state = seed)
+        centroid_mats.append(km._centroid_init(clusters)) #append 2D matrix with centroids to list
 
-	#assert initial centroids that came from initialization with different random seed are different
-	assert not np.allclose(centroid_mats[0],centroid_mats[1]), "Changing random seed did not change centroid initialization!"
-	assert not np.allclose(centroid_mats[0],centroid_mats[2]), "Changing random seed did not change centroid initialization!"
-	assert not np.allclose(centroid_mats[1],centroid_mats[2]), "Changing random seed did not change centroid initialization!"
+    #assert initial centroids that came from initialization with different random seed are different
+    assert not np.allclose(centroid_mats[0],centroid_mats[1]), "Changing random seed did not change centroid initialization!"
+    assert not np.allclose(centroid_mats[0],centroid_mats[2]), "Changing random seed did not change centroid initialization!"
+    assert not np.allclose(centroid_mats[1],centroid_mats[2]), "Changing random seed did not change centroid initialization!"
 
 
 def test_overfit_capability():
-	"""
-	As k increases, the training MSE should decrease because the algorithm will begin to overfit to the data. Here, I test that the training MSE
-	of the algorithm decreases as I re-fit the model on the same data with increasing k. I use loose clusters that are not easily distinguishable to 
-	demonstrate that increasing k will decrease training MSE even in cases that are difficult to cluster. 
-	"""
-	errors = []
-	for k in [3,10,25,50]:
-		clusters, labels = make_clusters(scale=2,k=4)
-		km = KMeans(k=k,random_state = 0)
-		km.fit(clusters)
-		errors.append(km.get_error())
+    """
+    As k increases, the training MSE should decrease because the algorithm will begin to overfit to the data. Here, I test that the training MSE
+    of the algorithm decreases as I re-fit the model on the same data with increasing k. I use loose clusters that are not easily distinguishable to 
+    demonstrate that increasing k will decrease training MSE even in cases that are difficult to cluster. 
+    """
+    errors = []
+    for k in [3,10,25,50]:
+        clusters, labels = make_clusters(scale=2,k=4)
+        km = KMeans(k=k,random_state = 0)
+        km.fit(clusters)
+        errors.append(km.get_error())
 
-	#assert training MSE decreases as you increase k
-	assert errors[0] > errors[1] > errors[2] > errors[3], "MSE does not decrease with increasing k!"
-	 
+    #assert training MSE decreases as you increase k
+    assert errors[0] > errors[1] > errors[2] > errors[3], "MSE does not decrease with increasing k!"
+     
 
 def test_check_easy_cluster2():
-	"""
-	This test checks to see that the KMeans algorithm properly clusters a very easily clusterable dataset.
-	It does this checking that the MSE of each point with its cluster centroid is low, which would suggest tight clustering,
-	which is expected with the easily clusterable data used in this test
-	This test will assert that MSE is less than the distance between the two further points. This will show that, in general, points are closer to their 
-	cluster centroids than two points that are relatively far apart. I will use the same k for KMeans clustering as there are clusters in the true data,
-	to avoid getting a low MSE due to overfitting with a high k
-	This also tests the _MSE method; if it works correctly it should give a small value here.
-	"""
-	k = 3
+    """
+    This test checks to see that the KMeans algorithm properly clusters a very easily clusterable dataset.
+    It does this checking that the MSE of each point with its cluster centroid is low, which would suggest tight clustering,
+    which is expected with the easily clusterable data used in this test
+    This test will assert that MSE is less than the distance between the two further points. This will show that, in general, points are closer to their 
+    cluster centroids than two points that are relatively far apart. I will use the same k for KMeans clustering as there are clusters in the true data,
+    to avoid getting a low MSE due to overfitting with a high k
+    This also tests the _MSE method; if it works correctly it should give a small value here.
+    """
+    k = 3
     clusters, labels = make_clusters(k=k,scale=0.3)
 
     #fit KMeans and predictions for these samples
     km = KMeans(k=k,random_state = 0)
-	km.fit(clusters)
-	pred = km.predict(clusters)
+    km.fit(clusters)
+    pred = km.predict(clusters)
 
-	max_distance = np.max(cdist(clusters,clusters).ravel()) #max distance between any two points in the dataset
-	training_mse = km.get_error()
-	assert training_mse < max_distance, "Your model was not able to properly cluster an easy dataset and get a low MSE"
+    max_distance = np.max(cdist(clusters,clusters).ravel()) #max distance between any two points in the dataset
+    training_mse = km.get_error()
+    assert training_mse < max_distance, "Your model was not able to properly cluster an easy dataset and get a low MSE"
 
 
 def test_label_assignment():
-	"""
-	Trying to predict labels from the same data that was used to fit the model should return the same labels that were assigned during the fitting process
-	If that is not the case, something has gone wrong either in the KMeans.fit or KMeans.predict methods.
-	Therefore, here I assert that the cluster assignments generated during training, stored in a private attribute KMeans._training_clusters,
-	are the same as the cluster assignments KMeans.predict produces on the data.
-	"""
-	k = 4
-	clusters, labels = make_clusters(scale=0.3,k=k)
-	km = KMeans(k=k,random_state = 0)
-	km.fit(clusters)
-	pred_labels = km.predict(clusters)
+    """
+    Trying to predict labels from the same data that was used to fit the model should return the same labels that were assigned during the fitting process
+    If that is not the case, something has gone wrong either in the KMeans.fit or KMeans.predict methods.
+    Therefore, here I assert that the cluster assignments generated during training, stored in a private attribute KMeans._training_clusters,
+    are the same as the cluster assignments KMeans.predict produces on the data.
+    """
+    k = 4
+    # Generate clustered data, train KMeans, get clustering predictions on the training data
+    clusters, labels = make_clusters(scale=0.3,k=k)
+    km = KMeans(k=k,random_state = 0)
+    km.fit(clusters)
+    pred_labels = km.predict(clusters)
 
-	assert km._training_clusters == pred_labels, "cluster assignments during fitting are different than predictions on the training data!"
+    assert km._training_clusters == pred_labels, "cluster assignments during fitting are different than predictions on the training data!"
 
--- test/test_silhouette.py
+++ test/test_silhouette.py
@@ -6,88 +6,88 @@ import pytest
 
 
 def test_silhouette():
-	"""
-	If the silhouette score was implemented properly, points near the center of a cluster should have higher scores than points that are near the edge of a cluster
-	Therefore, here I implement the KMeans algorithm, calculate the silhouette score on the predictions, and find the 5 points with the highest silhouette score
-	and the 5 points with the lowest silhouette scores. I then compare the average distance between these two sets of points to their closest cluster centroid.
-	If the score was implemented correctly, the 5 points with the highest silhouette scores should be closer on average to their cluster centroid than the 5 points with the lowest silhouette score.
-	Assert that is the case
+    """
+    If the silhouette score was implemented properly, points near the center of a cluster should have higher scores than points that are near the edge of a cluster
+    Therefore, here I implement the KMeans algorithm, calculate the silhouette score on the predictions, and find the 5 points with the highest silhouette score
+    and the 5 points with the lowest silhouette scores. I then compare the average distance between these two sets of points to their closest cluster centroid.
+    If the score was implemented correctly, the 5 points with the highest silhouette scores should be closer on average to their cluster centroid than the 5 points with the lowest silhouette score.
+    Assert that is the case
 
 
-	Here is how i do this:
-	1. Generate clustered data, perform fitting, perform predictions, calculate silhouette scores
-	2. Take each score in the silhouette scores array `scores` and place it inside a len(2) tuple with that scores index in `scores`. Store all of these 
-	tuples in `new_scores`.
-`	3. Sort new_scores and index the first 5 and last 5 scores to get the 5 smallest and 5 largest silhouette scores. I am able to reference the original index 
-	of these points in `scores` because of the second element in the tuple. This idx also corresponds to the observation's location in `clusters` (I test order preservation with a different unit test to confirm this)
-	4. Use these indices to get the 5 largest and 5 smallest points from `clusters`
-	5. Get the distance from all of these points to their closest cluster centroid, and find the average between the 5 points with the smallest silhouette score
-	and the 5 points with the largest silhouette score.
-	6. Assert that the 5 points with the largest silhouette score are closer on average to their closest centroid  than the 5 points with the smallest silhouette score
-	"""
+    Here is how i do this:
+    1. Generate clustered data, perform fitting, perform predictions, calculate silhouette scores
+    2. Take each score in the silhouette scores array `scores` and place it inside a len(2) tuple with that scores index in `scores`. Store all of these 
+    tuples in `new_scores`.
+    `	3. Sort new_scores and index the first 5 and last 5 scores to get the 5 smallest and 5 largest silhouette scores. I am able to reference the original index 
+    of these points in `scores` because of the second element in the tuple. This idx also corresponds to the observation's location in `clusters` (I test order preservation with a different unit test to confirm this)
+    4. Use these indices to get the 5 largest and 5 smallest points from `clusters`
+    5. Get the distance from all of these points to their closest cluster centroid, and find the average between the 5 points with the smallest silhouette score
+    and the 5 points with the largest silhouette score.
+    6. Assert that the 5 points with the largest silhouette score are closer on average to their closest centroid  than the 5 points with the smallest silhouette score
+    """
 
-	#Generate clustered data, train KMeans, cluster the data, calculate silhouette scores
-	clusters, labels = make_clusters(scale=1.5,m=3)
-	km = KMeans(k=3,random_state = 0)
-	km.fit(clusters)
-	pred = km.predict(clusters)
-	scores = Silhouette().score(clusters, pred)
-
-
-	new_scores = []
-	for idx,score in enumerate(scores):
-	    new_scores.append((score,idx)) #store each score as a tuple with its original index in a new list
-	    
-	new_scores.sort() #sort new list by the score so I can easily index out points with high and low silhouette score
-	min_scores = new_scores[:5]
-	max_scores = new_scores[-5:]
+    #Generate clustered data, train KMeans, cluster the data, calculate silhouette scores
+    clusters, labels = make_clusters(scale=1.5,m=3)
+    km = KMeans(k=3,random_state = 0)
+    km.fit(clusters)
+    pred = km.predict(clusters)
+    scores = Silhouette().score(clusters, pred)
 
 
-	original_min_idxs = []
-	for tup in min_scores:
-	    original_min_idxs.append(tup[1]) #get the original (pre-sorted) index of each of the 5 values
-	pts_with_min_scores = clusters[original_min_idxs,] #use the pre-sorted index to extract the original observation vectors
+    new_scores = []
+    for idx,score in enumerate(scores):
+        new_scores.append((score,idx)) #store each score as a tuple with its original index in a new list
+        
+    new_scores.sort() #sort new list by the score so I can easily index out points with high and low silhouette score
+    min_scores = new_scores[:5]
+    max_scores = new_scores[-5:]
 
-	#get the average distance of the 5 points with the smallest silhouette scores to their closest centroid
-	min_pts_avgdist_to_closest_centroid = np.mean(np.min(cdist(pts_with_min_scores,km.get_centroids()),axis=1))
-	    
-	
-	original_max_idxs = []
-	for tup in max_scores:
-	    original_max_idxs.append(tup[1])
-	pts_with_max_scores = clusters[original_max_idxs,]
 
-	#get the average distance of the 5 points with the largest silhouette scores to their closest centroid
-	max_pts_avgdist_to_closest_centroid = np.mean(np.min(cdist(pts_with_max_scores,km.get_centroids()),axis=1))
+    original_min_idxs = []
+    for tup in min_scores:
+        original_min_idxs.append(tup[1]) #get the original (pre-sorted) index of each of the 5 values
+    pts_with_min_scores = clusters[original_min_idxs,] #use the pre-sorted index to extract the original observation vectors
 
-	assert max_pts_avgdist_to_closest_centroid < min_pts_avgdist_to_closest_centroid, "Points with large silhouette scores are farther away from their closest centroid than points with small silhouette scores!"
+    #get the average distance of the 5 points with the smallest silhouette scores to their closest centroid
+    min_pts_avgdist_to_closest_centroid = np.mean(np.min(cdist(pts_with_min_scores,km.get_centroids()),axis=1))
+        
+
+    original_max_idxs = []
+    for tup in max_scores:
+        original_max_idxs.append(tup[1])
+    pts_with_max_scores = clusters[original_max_idxs,]
+
+    #get the average distance of the 5 points with the largest silhouette scores to their closest centroid
+    max_pts_avgdist_to_closest_centroid = np.mean(np.min(cdist(pts_with_max_scores,km.get_centroids()),axis=1))
+
+    assert max_pts_avgdist_to_closest_centroid < min_pts_avgdist_to_closest_centroid, "Points with large silhouette scores are farther away from their closest centroid than points with small silhouette scores!"
 
 
 def test_order_preservation():
-	"""
-	Test that the order the scores are returned from Silhouette.score are consistent with the order of observations given to Silhouette.score
+    """
+    Test that the order the scores are returned from Silhouette.score are consistent with the order of observations given to Silhouette.score
 
-	How I test for this:
-	1. Generate clustered data, perform fitting, perform predictions, calculate silhouette scores
-	3. recalculate the Silhouette score for every observation in the data using the private KMeans._get_silhouette_distances method
-		The order the scores are calculated in here are consistent with the order the observations appear in `pred`
-	4. Check each calculated score is identical (or approximately equal to what is returned in Silhouette.score() at the same index to ensure order is preserved
+    How I test for this:
+    1. Generate clustered data, perform fitting, perform predictions, calculate silhouette scores
+    3. recalculate the Silhouette score for every observation in the data using the private KMeans._get_silhouette_distances method
+    	The order the scores are calculated in here are consistent with the order the observations appear in `pred`
+    4. Check each calculated score is identical (or approximately equal to what is returned in Silhouette.score() at the same index to ensure order is preserved
 
-	"""
-	#Generate clustered data, train KMeans, cluster the data, calculate silhouette scores
-	clusters, labels = make_clusters(scale=1.5,m=3)
-	km = KMeans(k=3,random_state = 0)
-	km.fit(clusters)
-	pred = km.predict(clusters)
-	scores = Silhouette().score(clusters, pred)
+    """
+    #Generate clustered data, train KMeans, cluster the data, calculate silhouette scores
+    clusters, labels = make_clusters(scale=1.5,m=3)
+    km = KMeans(k=3,random_state = 0)
+    km.fit(clusters)
+    pred = km.predict(clusters)
+    scores = Silhouette().score(clusters, pred)
 
-	def approx_equal(a, b, allowed_error = 0.0001):
+    def approx_equal(a, b, allowed_error = 0.0001):
         return abs(a - b) < allowed_error
 
-	centroid_mat = km.get_centroids()
-	for obs in range(len(pred)):
-		mean_intra_distance,nearest_centroid_distance = self._get_silhouette_distances(X,y,unique_labels,centroid_mat,obs)
-		score = (nearest_centroid_distance - mean_intra_distance) / (max(nearest_centroid_distance,mean_intra_distance))
-		assert approx_equal(scores[obs],score), f"Re-calculated Silhouette Score is not the same as the model's Silhouette score for observation {obs}"
+    centroid_mat = km.get_centroids()
+    for obs in range(len(pred)):
+        mean_intra_distance,nearest_centroid_distance = self._get_silhouette_distances(X,y,unique_labels,centroid_mat,obs)
+        score = (nearest_centroid_distance - mean_intra_distance) / (max(nearest_centroid_distance,mean_intra_distance))
+        assert approx_equal(scores[obs],score), f"Re-calculated Silhouette Score is not the same as the model's Silhouette score for observation {obs}"
 
 
