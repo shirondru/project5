fb37db102255d48779ef22e41b37762116bddcdb
shirondru <59421145+shirondru@users.noreply.github.com>
2022-02-09 18:03:54 -0800

    Update comments and implement silhouette scores consistently with the wiki, not lecture slides

--- cluster/silhouette.py
+++ cluster/silhouette.py
@@ -34,8 +34,8 @@ class Silhouette:
         5. Loop through all observations and get the mean distance between that point and all other points in the same cluster. Also get the distance between the current point and the closest cluster centroid
             5a. Use cdist to get distances between current point and all points with the same label. This includes the distance between the current point and itself
                 5ab. The distance between the current point and itself = 0, so to get the mean distance between current point and other points in the same cluster sum the distances and divide by # of points in the cluster - 1
-            5b. Grab the centroid vectors for all centroids except the one from the same cluster as the current point from `centroid_mat`. Calculate the distance between the current point and all the other centroids using cdist.
-            Save the distance from the point to its closest centroid (from a different cluster)
+            5b. Loop through all cluster labels except the one corresponding to the current observation. In each iteration of the loop, find the mean distance between the current observation and all the points in other cluster. 
+            Append this mean distance to a list, and return the smallest mean distance after all iterations. This is the smallest distance between the current point and all points in another cluster
             5c. Calculate the silhouette score for this point
             5d. save the score in the 1D array, `scores`. Order will be preserved
         
@@ -65,17 +65,17 @@ class Silhouette:
             centroid_mat[idx,] = cluster_data.mean(axis=0) #store centroid for this label. Use idx (not label) for assignment in case labels are not contiguous and/or do not start at 0
         
         for obs in range(len(y)):
-            mean_intra_distance,mean_inter_distance = self._get_silhouette_distances(X,y,unique_labels,centroid_mat,obs)
+            mean_intra_distance,min_mean_inter_distance = self._get_silhouette_distances(X,y,unique_labels,centroid_mat,obs)
             
             ###calculate silhouette score and add it to scores array in the same order as the observations provided in y
-            score = (nearest_centroid_distance - mean_intra_distance) / (max(nearest_centroid_distance,mean_intra_distance))
+            score = (min_mean_inter_distance - mean_intra_distance) / (max(min_mean_inter_distance,mean_intra_distance))
             scores[obs] = score
         return scores
 
     def _get_silhouette_distances(self,X: np.ndarray, y: np.ndarray, unique_labels: list,centroid_mat: np.ndarray, obs: int):
 
         """
-        For the given observation, `obs` get the the mean intra-cluster distance and the distance between this point and it's nearest cluster centroid.
+        For the given observation, `obs` get the the mean intra-cluster distance and smallest distance between this point and all other points in another cluster.
         Details for what this method is doing can be found in Steps 5-5b in the docstring of `self.score`
 
 
@@ -109,17 +109,13 @@ class Silhouette:
         intra_distances = cdist(obs_data,rest_of_cluster_data,metric = self._metric) #get distance between this obs and all obs with same label
         mean_intra_distance = np.sum(intra_distances) / (intra_distances.shape[1] - 1) #intra_distance includes the distance of current obs to itself, so divide by length of points in the cluster minus one to not include that in the average
 
-        #### Get distance between current point and closest label/cluster centroid
-        other_cluster_centroid_rows = [x for x in range(len(unique_labels)) if x != unique_labels.index(obs_label)] #this is a list corresponding to all other centroid rows in centroid_mat
-        other_cluster_centroids = centroid_mat[other_cluster_centroid_rows,] 
-        cluster_distances  = cdist(obs_data, other_cluster_centroids) #distance between current point and all other label/cluster centrouds
-        nearest_centroid_idx = np.argmin(cluster_distances) 
-        nearest_cluster = other_cluster_centroid_rows[nearest_centroid_idx] #the label corresponding to the cluster with the closest centroid
-        pts_in_nearest_cluster = X[y==nearest_cluster,]
-        mean_inter_distance = np.mean(cdist(obs_data,pts_in_nearest_cluster))
-        # nearest_centroid_distance = inter_distances[0,nearest_centroid_idx] 
+        #get minimum mean distance of current observation to all points in any of the other clusters
+        mean_inter_distances = []
+        for other_label in [x for x in unique_labels if x != obs_label]: #loop through labels corresponding to other clusters
+            mean_inter_distances.append(np.mean(cdist(obs_data,X[y==other_label]))) #append mean distance of current point to all points in other cluster to list
+        min_mean_inter_distance = min(mean_inter_distances) 
     
-        return mean_intra_distance,mean_inter_distance
+        return mean_intra_distance,min_mean_inter_distance
 
     
     
